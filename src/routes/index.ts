876d266fa6c1fc91fb8432defe9f19eb67e9909d
Jared Dantis <jareddantis@gmail.com>
2019-06-24 21:49:21 +0800

    routes: Dynamically import Calc, too

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Calc from './Calc.vue'
 
 Vue.use(Router)
 
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'calc',
-      component: Calc,
+      component: () => import(/* webpackChunkName: "calc" */ './Calc.vue'),
     },
     {
       path: '/transmute',
