b54f60fa9009f21ee50e6abe63f87372a2b6b594
Jared Dantis <jareddantis@gmail.com>
2019-06-24 22:53:06 +0800

    Try to fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,11 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
   },
   transformIgnorePatterns: [
-    '/node_modules/'
+    '<rootDir>/node_modules'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
