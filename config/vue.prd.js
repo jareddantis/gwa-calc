19adae5cbb8ca78a67e0355ea5a9f80e0207c770
Jared Dantis <jareddantis@gmail.com>
2019-07-30 21:02:32 +0800

    config: Vuetify à la carte doesn't like explicit vue-loader usage

--- config/vue.prd.js
+++ config/vue.prd.js
@@ -56,11 +56,6 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-          include: [ path.resolve( __dirname, 'src') ],
-        },
-        {
           test: /\.(js|ts|tsx)$/,
           loader: 'babel-loader',
           exclude: [ path.resolve(__dirname, '../node_modules/qr-scanner/qr-scanner-worker.min.js') ],
