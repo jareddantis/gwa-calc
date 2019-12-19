f4f94184e607aebe3592a7321f27225ff772004f
Jared Dantis <jareddantis@gmail.com>
2019-12-19 16:21:28 +0800

    chore: Update packages

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset',
+    '@babel/typescript'
+  ]
+};
+
+// module.exports = {
+//   "presets": [
+//     ["@vue/app", { "useBuiltIns": "entry" }],
+//     "@babel/env",
+//     "@babel/typescript"
+//   ]
+// };
