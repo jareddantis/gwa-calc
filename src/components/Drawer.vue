e827eaeaa30ee6112552a4bb5e738c876260f4ab
Jared Dantis <jareddantis@gmail.com>
2019-12-20 15:37:49 +0800

    Drawer: Add header image

---
+++ src/components/Drawer.vue
@@ -0,0 +1,122 @@
+<template>
+  <!-- Navigation drawer -->
+  <v-navigation-drawer app temporary v-model="visible" :width="300">
+    <v-img :src="isDarkMode ? darkWaves : lightWaves" aspect-ratio="1.5">
+      <div :class="`header ml-4 ${isDarkMode ? '' : 'black--text'}`">
+        <h3 class="subtitle-2">GWA Calc {{ appVersion ? `build ${appVersion}` : 'debug build' }}</h3>
+        <p class="caption">&copy; 2015-2020 Jared Dantis<br>Licensed under AGPLv3</p>
+      </div>
+    </v-img>
+    <v-list nav rounded flat dense>
+      <v-subheader class="overline">Subject sets</v-subheader>
+      <v-list-item>
+        <v-select :items="subjects" :value="currentSet" @change="setChanged"
+                  :dark="isDarkMode" color="orange" label="Current"
+                  outlined hide-details class="mt-2 mb-4"/>
+      </v-list-item>
+      <v-list-item-group>
+        <v-list-item @click="$bus.$emit('show-set-manager-dialog')">
+          <v-list-item-icon><v-icon>add</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Create or manage sets</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$bus.$emit('show-set-encoder-dialog')">
+          <v-list-item-icon><v-icon>share</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Share set</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$bus.$emit('show-set-decoder-dialog')">
+          <v-list-item-icon><v-icon>camera_alt</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Import set</v-list-item-title></v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+
+      <v-subheader class="overline mt-4">Tools</v-subheader>
+      <v-list-item-group>
+        <v-list-item v-show="!isHiddenPshs" @click="$bus.$emit('show-transmute-dialog')">
+          <v-list-item-icon><v-icon>merge_type</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Transmute PSHS grades</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$store.commit('updateDarkMode', !isDarkMode)">
+          <v-list-item-icon><v-icon>brightness_3</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Toggle dark mode</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$store.commit('updateHiddenPshs', !isHiddenPshs)">
+          <v-list-item-icon><v-icon>school</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Toggle PSHS features</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$bus.$emit('delete-all-data')">
+          <v-list-item-icon><v-icon>delete_forever</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Delete all data</v-list-item-title></v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+
+      <v-divider class="my-6"/>
+      <v-list-item-group>
+        <v-list-item>
+          <v-list-item-icon><v-icon>monetization_on</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Donate</v-list-item-title></v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="sendFeedback">
+          <v-list-item-icon><v-icon>comment</v-icon></v-list-item-icon>
+          <v-list-item-content><v-list-item-title class="body-2">Send feedback</v-list-item-title></v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/components/Drawer';
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { mapState } from 'vuex'
+
+@Component({
+  computed: mapState(['isDarkMode', 'isHiddenPshs']),
+})
+export default class Drawer extends Vue {
+  public isDarkMode!: boolean
+  public isHiddenPshs!: boolean
+  public visible: boolean = false
+  private darkWaves = require('../assets/wavy-dark.svg')
+  private lightWaves = require('../assets/wavy.svg')
+
+  public created() {
+    this.$bus.$on('open-drawer', () => this.visible = true)
+  }
+
+  public setChanged(set: string) {
+    this.$store.dispatch('updateCurrentSet', set)
+    this.visible = false
+  }
+
+  public sendFeedback() {
+    const link = `mailto:jareddantis@gmail.com?subject=GWACalc-Vue-${this.appVersion || 'debug'}`
+    window.open(link, '_blank', 'noopener')
+    this.visible = false
+  }
+
+  get appVersion(): string {
+    return this.$store.getters.appVersion
+  }
+  get currentSet(): string {
+    return this.$store.getters.currentSet
+  }
+  get subjects(): string[] {
+    let subjects: string[] = []
+    const customSets = this.$store.getters.customSets
+
+    if (customSets.length > 0) {
+      subjects = subjects.concat(customSets)
+    }
+
+    if (!this.isHiddenPshs) {
+      subjects = subjects.concat(this.$store.getters.pshsSets)
+    }
+
+    return subjects
+  }
+}
+</script>
